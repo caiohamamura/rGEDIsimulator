40c8c0a49d5969b9ab895f36a45f074575868519
caiohamamura <caiohamamura@gmail.com>
2024-04-22 15:45:25 -0400

    * Use pkg-config to configure libraries

--- R/zzz.R
+++ R/zzz.R
@@ -2,3 +2,14 @@
   library.dynam.unload("rGEDIsimulator", libpath)
   invisible()
 }
+
+.onAttach <- function(libname, pkgname) {
+  if (Sys.info()[["sysname"]] == "Windows") {
+    if (Sys.getenv("PROJ_LIB") == "") {
+      Sys.setenv("PROJ_LIB" = system.file("proj", package = pkgname)[1])
+    }
+    if (Sys.getenv("GDAL_DATA") == "") {
+      Sys.setenv("GDAL_DATA" = system.file("gdal", package = pkgname)[1])
+    }
+  }
+}
